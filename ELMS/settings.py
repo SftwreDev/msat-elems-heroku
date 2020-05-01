db8643ac027d896dcf3b51e5fb954987ec023100
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-05-02 00:14:23 +0800

    Update settings.py

--- ELMS/settings.py
+++ ELMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't^wz&m=zfrec!r6hj&hr4nedy^_c*0*j6nrs$rfv-kl)^%q_#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["msat-elms.herokuapp.com" ,'127.0.0.1']
 
